20f2f0345bc42802d899e7873236ec9ca620e4d4
kyileeyu <kyileeeyu@gmail.com>
2025-07-13 10:17:06 +0900

    feat: 초기 진입 라우터 설정

---
+++ src/pages/book.tsx
@@ -0,0 +1,15 @@
+const Book = () => {
+
+
+    return (
+        <div
+            className={` grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+        >
+        <h1>Book Page</h1>
+        <p>This is the book page content.</p>
+            보이니?
+        </div>
+    );
+}
+
+export default Book;
