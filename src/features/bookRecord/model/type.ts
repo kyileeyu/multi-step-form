3466139af0ebbe40c56dc3495efa17d51f8b0b33
kyileeyu <kyileeeyu@gmail.com>
2025-07-14 21:22:45 +0900

    feat: 독서 기본정보 페이지 입력 폼 생성 및 유효성 검사 처리

--- src/features/bookRecord/model/type.ts
+++ src/features/bookRecord/model/type.ts
@@ -1,9 +1,17 @@
+export const BOOK_RECORD_STATUS = [
+  { value: "READ", label: "읽음" },
+  { value: "READING", label: "읽는 중" },
+  { value: "PENDING", label: "읽을 예정" },
+  { value: "WISHLIST", label: "위시리스트" },
+];
+
 export type BookRecordType = {
   id: string;
   title: string;
   author: string;
   pageCount: number;
-  status: "READ" | "READING" | "PENDING" | "WISHLIST";
+  publishDate: string;
+  status: (typeof BOOK_RECORD_STATUS)[number]["value"];
   startDate: string;
   endDate: string;
   //
--- src/features/bookRecord/ui/BasicInfo.tsx
+++ src/features/bookRecord/ui/BasicInfo.tsx
@@ -1,11 +1,106 @@
+import { Controller, useFormContext } from "react-hook-form";
+import {
+  BOOK_RECORD_STATUS,
+  BookRecordType,
+} from "@/features/bookRecord/model/type";
+
 export const BasicInfo = () => {
-    return (
-        <div className="flex flex-col gap-4">
-        <h2 className="text-lg font-semibold">Basic Information</h2>
-        <p className="text-sm text-gray-600">
-            This section contains the basic information about the book record.
-        </p>
-        {/* Additional content can be added here */}
-        </div>
-    );
-}
+  const { control } = useFormContext<BookRecordType>();
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-lg font-semibold">책 기본 정보</h2>
+      <p className="text-sm text-gray-600">기본 정보를 입력해주세요</p>
+      <div className={"flex flex-col gap-5"}>
+        <Controller
+          name={"title"}
+          control={control}
+          rules={{ required: "제목은 필수입니다." }}
+          render={(field) => <input {...field} placeholder="제목" />}
+        />
+        <Controller
+          name={"publishDate"}
+          control={control}
+          rules={{ required: "출판일은 필수입니다." }}
+          render={({ field }) => (
+            <input {...field} type="date" placeholder="도서 출판일" />
+          )}
+        />
+        <Controller
+          name={"status"}
+          control={control}
+          rules={{ required: "상태는 필수입니다." }}
+          render={({ field }) => (
+            <select {...field} id="status">
+              <option disabled selected>
+                상태
+              </option>
+              {BOOK_RECORD_STATUS.map((status) => (
+                <option key={status.value} value={status.value}>
+                  {status.label}
+                </option>
+              ))}
+            </select>
+          )}
+        />
+        <Controller
+          name={"startDate"}
+          control={control}
+          rules={{
+            //그냥 입력을 막아버리면 안될까?
+            validate: (value, vals) => {
+              const status = vals.status;
+              if (status === "WISHLIST" && value) {
+                return "독서 시작일은 입력할 수 없습니다.";
+              }
+              if (!value) {
+                return "독서 시작일은 필수입니다.";
+              }
+              return true;
+            },
+          }}
+          render={({ field, fieldState: { error } }) => (
+            <>
+              <input {...field} type="date" placeholder="독서 시작일" />
+              {error && <p>{error.message}</p>}
+            </>
+          )}
+        />
+        <Controller
+          name={"endDate"}
+          control={control}
+          rules={{
+            validate: (value, vals) => {
+              const status = vals.status;
+              if (value && (status === "WISHLIST" || status === "PENDING")) {
+                return "독서 종료일은 입력할 수 없습니다.";
+              }
+              if (!value) {
+                return "독서 종료일은 필수입니다.";
+              }
+              if (
+                vals.startDate &&
+                new Date(value) < new Date(vals.startDate)
+              ) {
+                return "독서 종료일은 시작일 이후여야 합니다.";
+              }
+              if (
+                vals.publishDate &&
+                new Date(value) < new Date(vals.publishDate)
+              ) {
+                return "독서 종료일은 출판일 이후여야 합니다.";
+              }
+              return true;
+            },
+          }}
+          render={({ field, fieldState: { error } }) => (
+            <>
+              <input {...field} type="date" placeholder="독서 종료일" />
+              {error && <p>{error.message}</p>}
+            </>
+          )}
+        />
+      </div>
+    </div>
+  );
+};
