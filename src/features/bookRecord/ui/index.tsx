5b1d216b5b1e92a56828475ee628003a168b1d46
kyileeyu <kyileeeyu@gmail.com>
2025-07-14 20:16:29 +0900

    chore: prettier 추가

--- src/features/bookRecord/ui/index.tsx
+++ src/features/bookRecord/ui/index.tsx
@@ -1,5 +1,6 @@
-export {BasicInfo} from './BasicInfo';
-export {Quotes} from './Quotes';
-export {BookRecord} from './BookRecord';
-export {Rating} from './Rating';
-export {PublicOrNot} from './PublicOrNot';
+export { BasicInfo } from "./BasicInfo";
+export { Quotes } from "./Quotes";
+export { BookRecord } from "./BookRecord";
+export { Rating } from "./Rating";
+export { PublicOrNot } from "./PublicOrNot";
+export { FunnelLayout } from "./FunnelLayout";
