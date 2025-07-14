0c8abce770aa4c83e1e8246936128f3d54502433
kyileeyu <kyileeeyu@gmail.com>
2025-07-14 21:49:48 +0900

    feat: 별점 및 독후감 페이지 필드 구현

--- src/features/bookRecord/ui/BookRecord.tsx
+++ src/features/bookRecord/ui/BookRecord.tsx
@@ -1,11 +1,34 @@
-export const BookRecord = ()   =>{
-    return (
-        <div className="flex flex-col gap-4">
-            <h1 className="text-2xl font-bold">Book Record</h1>
-            <p className="text-sm text-gray-600">
-                This section contains the book record details.
-            </p>
-            {/* Additional content can be added here */}
-        </div>
-    );
-}
+import { Controller, useFormContext } from "react-hook-form";
+
+export const BookRecord = () => {
+  const { control } = useFormContext();
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-lg font-semibold">평점</h2>
+      <p className="text-sm text-gray-600">책을 평가해주세요</p>
+      <div className={"flex flex-col gap-5"}>
+        <Controller
+          name={"record"}
+          control={control}
+          rules={{
+            required: "제목은 필수입니다.",
+            validate: (value, vals) => {
+              const rating = vals.rating;
+              if ((rating <= 1 || rating >= 5) && value.length < 100) {
+                return "100자 이상의 이유를 입력해주세요.";
+              }
+              return true;
+            },
+          }}
+          render={({ field, fieldState: { error } }) => (
+            <label>
+              <textarea {...field} rows={5} />
+              {error && <span className="text-red-500">{error.message}</span>}
+            </label>
+          )}
+        />
+      </div>
+    </div>
+  );
+};
--- src/features/bookRecord/ui/Rating.tsx
+++ src/features/bookRecord/ui/Rating.tsx
@@ -1,8 +1,30 @@
-export const Rating = () => {
-    return (
-        <div className="flex flex-col gap-4">
-        <h2 className="text-lg font-semibold">Rating</h2>
+import { Controller, useFormContext } from "react-hook-form";
+import { Rating as StarRating } from "@mui/material";
 
-        </div>
-    );
-}
+export const Rating = () => {
+  const { control } = useFormContext();
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-lg font-semibold">평점</h2>
+      <p className="text-sm text-gray-600">책을 평가해주세요</p>
+      <div className={"flex flex-col gap-5"}>
+        <Controller
+          name={"isRecommended"}
+          control={control}
+          render={(field) => (
+            <label>
+              이 책을 추천합니다
+              <input {...field} type={"checkbox"} />
+            </label>
+          )}
+        />
+        <Controller
+          name={"rating"}
+          control={control}
+          render={({ field }) => <StarRating precision={0.5} {...field} />}
+        />
+      </div>
+    </div>
+  );
+};
